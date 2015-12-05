6c51b0e0b5463714697ecfe37da4d3361d417107
Edwin Efraín Jiménez Lepe <eejl_1992@hotmail.com>
2015-12-05 14:21:23 -0600

    actualizacion
    separacion de simbolos en las gramaticas con simbolo |

--- dataStructures.h
+++ dataStructures.h
@@ -3,6 +3,8 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 //http://www.cplusplus.com/reference/
 
@@ -66,7 +68,73 @@ class Gramar{
             else return std::vector<std::string>();
 
         }
+        //get production for key, without the symbol '|'
+        std::vector<std::string> getProductionClean(std::string key){
+            if (symbols.find(key)!=symbols.end()){
+                symbol aux= symbols[key];
+                if (symbols[key].terminal){
+                    return std::vector<std::string>();
+                }
+                //Remove de | character
+                std::vector<std::string> auxProduction =symbols[key].productions;
+                int i=0,j=0;
 
+                 for(i=0;i<auxProduction.size();i++){
+                    while(j<auxProduction[i].size()){
+                        if(auxProduction[i].at(j)=='|'){// constante de separacion de simbolos en un produccion
+                            auxProduction[i].erase(auxProduction[i].begin()+j);
+                            j=0;
+                        }
+                        j++;
 
+                    }
+                 }
+                return auxProduction;
+
+            }
+            else return std::vector<std::string>();
+
+        }
+
+        std::vector<std::string> symbolsOfProduction(std::string productionString){
+
+            //Remove de | character
+            std::vector<std::string> auxProduction;
+            std::string subString;
+            int j=0;
+
+            while(j<productionString.size()){
+                if(productionString.at(j)=='|'){// constante de separacion de simbolos en un produccion
+                    auxProduction.push_back(subString);
+                    subString.clear();
+                }else{
+                    subString+=productionString.at(j);
+                }
+                j++;
+             }
+            return auxProduction;
+        }
+
+        std::vector<std::string> getStringsOfGramar(){
+            std::map<std::string, symbol>::iterator it;
+            std::vector<symbol> auxSymbol;
+            std::vector<std::string> strings;
+            it = symbols.begin();
+            int i,j ;
+            while ( it != symbols.end()){
+
+              auxSymbol.push_back(it->second);
+              it++;
+            }
+            for(i=0;i<auxSymbol.size();i++){
+                for(j=0;j<auxSymbol[i].productions.size();j++){
+                    strings.push_back( auxSymbol[i].name+"->"+auxSymbol[i].productions[j]);
+                }
+
+            }
+            return strings;
+        }
 
 };
+
+
--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "dataStructures.h"
 
 using namespace std;
@@ -12,24 +11,25 @@ int main()
 
 
     //insertar al final del vector
-    terminal.push_back ("a");//esto es una emulacion de lo que se tiene que insertar por interfaz
-    terminal.push_back ("b");
+    terminal.push_back ("a|");//esto es una emulacion de lo que se tiene que insertar por interfaz
+    terminal.push_back ("b|");
 
     nonTerminal.push_back("A");
         std::vector<string> auxA;// ESTAS SON LAS PRODUCCIONES DE A
-        auxA.push_back("a");
-        auxA.push_back("aA");
-    nonTerminal.push_back("B");
+        auxA.push_back("a|");
+        auxA.push_back("a|A|");// LA NOTACION DE " | " ES PARA DISTINGUIR ENTRE SIMBOLOS COMO A_b (a sub b) y simbolos de A al momento de leer
+        auxA.push_back("a|A|B|b|");
+    nonTerminal.push_back("B|");
         std::vector<string> auxB;// ESTAS SON LAS PRODUCCIONES DE B
-        auxB.push_back("b");
-        auxB.push_back("bB");
+        auxB.push_back("b|");
+        auxB.push_back("b|B|");
 
     //instancia de la gramatica
     Gramar G;
     G.defineInitial("S");// EL SIMBOLO POR DEFAULT ES S PERO LO AGREGO AQUI PARA DAR UN EJEMPLO DE COMO FUNCIONA ESTA FUNCION
         std::vector<string> auxS;// ESTAS SON LAS PRODUCCIONES DE
-        auxS.push_back("@");//Arroba es epsilon namas por que se me antojo
-        auxS.push_back("AB");
+        auxS.push_back("@|");//Arroba es epsilon namas por que se me antojo
+        auxS.push_back("A|B");
 
     G.insertNonTerminal(G.initial,std::vector<std::string>()); // EN ESTE PASO SE INSERTA UN NO TERMINAL, AGREGO "S" Y  UN VECTOR VACIO
     G.insertNonTerminal("A",auxA); //SE INSERTA UN NO TERMINAL Y SE LE ENVIA SUS PRODUCCIONES DIRECTAMENTE
@@ -46,19 +46,30 @@ int main()
 
 
 
-
     std::cout << "Que hay en auxA [0]?: " << auxA.at(1);// PREGUNTAR POR UN VECTOR
-    std::cout << "\nQue hay en auxA [0][1]?: " << auxA.at(1).at(1);// Cual es su segundo caracter?
+    std::cout << "\nQue hay en auxA [0][2]?: " << auxA.at(1).at(2);// Cual es su segundo caracter?
     std::cout << "\nProducciones del simbolo A ?: " << G.getProduction(G.initial).size();
+    for(i=0;i<G.getProduction("A").size();i++){ // Producciones de A ?
+        std::cout << "\n\t" << G.getProductionClean("A").at(i);
+    }
+
+    std::cout << "\nProducciones del simbolo A separadas por simbolo en la produccion ?: " ;
     for(i=0;i<G.getProduction("A").size();i++){ // Producciones de A ?
         std::cout << "\n\t" << G.getProduction("A").at(i);
     }
 
+    std::cout << "\nQue simbolos hay en la ultima produccion de A ?: \n" ;
+    for(i=0;i<G.symbolsOfProduction(G.getProduction("A").at(2)).size();i++){ // Producciones de A ?
+        std::cout << "\t" << G.symbolsOfProduction( G.getProduction("A").at(2) ).at(i);
+    }
+
     std::cout << "\nProducciones del simbolo inicial ?: " << G.initial << " -> ";
     for(i=0;i<G.getProduction(G.initial).size();i++){ // Producciones de A ?
-        std::cout << " \\ " << G.getProduction(G.initial).at(i);
+        std::cout << " \\ " << G.getProductionClean(G.initial).at(i);
     }//G.symbols[terminal[i]].productions=auxA;
 
+
+    printf("\n\n");
 
 
 }
